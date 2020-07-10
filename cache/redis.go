4ff94dea29d687ae61e05d1881bc86692d92796c
dongxiem <dongxiem@gmail.com>
2020-07-10 18:07:00 +0800

    添加Redis

---
+++ cache/redis.go
@@ -0,0 +1,52 @@
+package cache
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+var (
+	pool      *redis.Pool
+	redisHost = "127.0.0.1:6379"
+	redisPass = "testupload"
+)
+
+func NewRedisPool() *redis.Pool {
+
+	return &redis.Pool{
+		MaxIdle:     50,
+		MaxActive:   30,
+		IdleTimeout: 300 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			// 1.打开连接
+			c, err := redis.Dial("tcp", redisHost)
+			if err != nil {
+				fmt.Println(err)
+				return nil, err
+			}
+
+			// 2.访问认证
+			if _, err = c.Do("AUTH", redisPass); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(conn redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := conn.Do("PING")
+			return err
+		},
+	}
+}
+
+func init() {
+	pool = NewRedisPool()
+}
+
+func RedisPool() *redis.Pool {
+	return pool
+}
