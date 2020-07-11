6926910b3fc4ddf877d6ebe0d8706e20aa756b7a
dongxiem <ljmiaw@163.com>
2020-07-11 20:59:11 +0800

    添加文件信息及文件元信息对于的数据库操作

---
+++ db/file.go
@@ -0,0 +1,113 @@
+package db
+
+import (
+	mydb "carrotCloud/db/mysql"
+	"database/sql"
+	"fmt"
+)
+
+// TableFile : 文件表结构体
+type TableFile struct {
+	FileHash string
+	FileName sql.NullString
+	FileSize sql.NullInt64
+	FileAddr sql.NullString
+}
+
+// GetFileMeta : 从mysql当中获取文件元信息
+func GetFileMeta(fileHash string) (*TableFile, error) {
+
+	stmt, err := mydb.DBConn().Prepare(
+		"select file_sha1, file_addr, file_name, file_size from tbl_file" +
+			"where file_sha1=? and status=1 limit 1")
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	defer stmt.Close()
+
+	// 新建一个空的TableFile结构体用来接收查询返回的数据
+	tFile := TableFile{}
+	// 进行查询，得到一行数据，并存入结构体当中
+	err = stmt.QueryRow(fileHash).Scan(
+		&tFile.FileHash, &tFile.FileAddr, &tFile.FileName, &tFile.FileSize)
+	// 进行错误信息处理
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// 查不到对应的记录，返回参数及错误都为nil
+			return nil, nil
+		} else {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+	}
+	return &tFile, nil
+}
+
+// GetFileMetaList : 从mysql当中根据limit的数目批量获取文件元信息
+func GetFileMetaList(limit int) ([]TableFile, error) {
+	stmt, err := mydb.DBConn().Prepare(
+		"select file_sha1, file_addr, file_name, file_size, from tbl_file" +
+			"where status=1 limit?")
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	defer stmt.Close()
+	// 进行查询并返回多行数据
+	rows, err := stmt.Query(limit)
+	// 进行错误处理
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	columns, _ := rows.Columns()
+	values := make([]sql.RawBytes, len(columns))
+	// 新建一个空的TableFile用来存储并返回
+	var tFiles []TableFile
+	// 将每行数据进行处理
+	for i := 0; i < len(values) && rows.Next(); i++ {
+		tFile := TableFile{}
+		err = rows.Scan(&tFile.FileHash, &tFile.FileAddr,
+			&tFile.FileName, &tFile.FileSize)
+		if err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+		tFiles = append(tFiles, tFile)
+	}
+	// 打印接收了多少行数据
+	fmt.Println(len(tFiles))
+	// 将数据返回
+	return tFiles, nil
+}
+
+// OnFileUploadFinished : 文件上传完成，保存meta
+func OnFileUploadFinished(fileHash, fileName string, fileSize int64, fileAddr string) bool {
+	// Mysql语句欲写入，注意status为1
+	stmt, err := mydb.DBConn().Prepare(
+		"insert ignore into table_file (`file_sha1`, `file_name`, `file_size`," +
+			"`file_add`, `status`) values(?,?,?,?,1)")
+	// 错误处理
+	if err != nil {
+		fmt.Println("Failed to prepare statement, err:" + err.Error())
+		return false
+	}
+	defer stmt.Close()
+	// 传入参数并且执行语句
+	ret, err := stmt.Exec(fileHash, fileName, fileSize, fileAddr)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	// 返回影响行数，如果小于0则证明插入失败
+	if rf, err := ret.RowsAffected(); nil == err {
+		if rf <= 0 {
+			fmt.Printf("File with hash:%s has been uploaded before", fileHash)
+		}
+		return true
+	}
+	return false
+
+}
---
+++ db/userfile.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	mydb "carrotCloud/db/mysql"
+	"fmt"
+	"time"
+)
+
+// UserFile : 用户文件表结构体
+type UserFile struct {
+	UserName    string // 比meta原信息多了一个UserName，用了保存是哪个用户
+	FileHash    string
+	FileName    string
+	FileSize    int64
+	UploadAt    string
+	LastUpdated string
+}
+
+// OnUserFileUploadFinished ： 更新文件用户表
+func OnUserFileUploadFinished(userName, fileHash, fileName string, fileSize int64) bool {
+
+	// 语句预执行将数据插入表中
+	stmt, err := mydb.DBConn().Prepare(
+		"insert  ignore into tbl_user_tile(`user_name`, `file_sha`, `file_name`," +
+			"`file_size`, `upload_at`) values (?,?,?,?,?)")
+	if err != nil {
+		return false
+	}
+
+	defer stmt.Close()
+	// 将传入的参数进行上面准备语句的执行
+	_, err = stmt.Exec(userName, fileHash, fileName, fileHash, time.Now())
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// QueryUserFileMetas : 批量获取用户的文件信息
+func QueryUserFileMetas(userName string, limit int) ([]UserFile, error) {
+
+	stmt, err := mydb.DBConn().Prepare(
+		"select file_sha1, file_name, file_size, uploat_at," +
+			"last_update from tbl_user_file where user_name = ? limit ?")
+	if err != nil {
+		// 如果发生错误，则返回该错误
+		return nil, err
+	}
+	// 进行语句查询并返回多行数据
+	rows, err := stmt.Query(userName, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+	// 定义一个UserFile的用以接受返回的用户信息
+	var userFiles []UserFile
+
+	for rows.Next() {
+		// 定义一个零时UserFile结构体
+		uFile := UserFile{}
+		// 将每行的数据进行赋值
+		err = rows.Scan(&uFile.FileHash, &uFile.FileName, &uFile.FileSize,
+			&uFile.UploadAt, &uFile.LastUpdated)
+		// 发生了错误则打印并且break
+		if err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+		// 将每行数据插入到userFiles切片当中
+		userFiles = append(userFiles, uFile)
+	}
+	// 最后返回该切片，并且error为nil
+	return userFiles, nil
+}
