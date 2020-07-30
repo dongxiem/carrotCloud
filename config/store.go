7b62253552be290acd22c2f2e2427e8d17e46014
dongxiem <ljmiaw@163.com>
2020-07-30 11:37:20 +0800

    添加文件上传及下载的配置文件

--- config/store.go
+++ config/store.go
@@ -5,6 +5,14 @@ import (
 )
 
 const (
-	// 设置当前文件的存储类型
+	// TempLocalRootDir : 本地临时存储地址的路径
+	TempLocalRootDir = "D:/Garmen/tmp/fileserver/"
+	// TempPartRootDir : 分块文件在本地临时存储地址的路径
+	TempPartRootDir = "D:/Garmen/tmp/fileserver_part/"
+	// CephRootDir : Ceph的存储路径prefix
+	CephRootDir = "/ceph"
+	// OSSRootDir : OSS的存储路径prefix
+	OSSRootDir = "oss/"
+	// CurrentStoreType : 设置当前文件的存储类型
 	CurrentStoreType = cmn.StoreLocal
 )
